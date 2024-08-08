5b03d5f49e4ad128c10a73f4503740f584787682
Shane Shaji Thomas <shaneshaji10@gmail.com>
2024-08-08 13:10:07 +0530

    added api.js to handle API calls

---
+++ app/api/api.js
@@ -0,0 +1,14 @@
+export const fetchUsers = async () => {
+  try {
+    const response = await fetch(
+      "https://random-data-api.com/api/users/random_user?size=80"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("\nFailed to fetch users because of error -", error.message);
+  }
+};
